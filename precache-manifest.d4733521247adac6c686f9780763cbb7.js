02bea72bcb4d4ed133a365c3c51f48091a2182b5
Alex Louzada <alex.louzada@qualidata.com.br>
2019-09-12 14:31:37 -0300

    Build feito dia 12/09/2019 às 14:31

---
+++ precache-manifest.d4733521247adac6c686f9780763cbb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "766e06c43be2b50da7b1",
+    "url": "/js/chunk-vendors.706f38d4.js"
+  },
+  {
+    "revision": "9c7766a6114d8bd938bc",
+    "url": "/js/app.b0f43670.js"
+  },
+  {
+    "revision": "b7a70621183a03182b23",
+    "url": "/js/about.4f3505b2.js"
+  },
+  {
+    "revision": "3ed6610bc865e870ad8e544786e1980f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "766e06c43be2b50da7b1",
+    "url": "/css/chunk-vendors.aba34d16.css"
+  }
+];
